586ce6e0d8c0c2d8c7bf15e6dd539139db8e0d54
praveenaaa <pravinsingh918@gmail.com>
2019-12-19 10:38:29 +0530

    comments

--- src/test/java/com/cucumber_17_dec_pack1/TestRunner.java
+++ src/test/java/com/cucumber_17_dec_pack1/TestRunner.java
@@ -3,10 +3,11 @@ import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@CucumberOptions(plugin = {"json:target/cucumber.json"},
 		features = "Feature"
 		,glue={"step_definition"}
 		)
+
 public class TestRunner {
 
 }
